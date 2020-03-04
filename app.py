e60920bedc8d808bcdcb23daaff01ed20498f256
s1900570 <pia.tajanen-doumbouya@edu.bc.fi>
2020-03-04 19:22:20 +0200

    Exercises 1-7

--- app.py
+++ app.py
@@ -4,9 +4,48 @@ print("    /  |")
 print("   /   |")
 print("  /    |")
 print(" /_____|")
+
 character_name = "John"
 character_age = "35"
+is_male = False
 print("There once was a man named " + character_name + " ,")
 print("he was " + character_age + " years old. ")
+print("\n")
+character_name = "Joe"
 print("He really liked the name " + character_name + ",")
 print("but didn't like being " + character_age + ".")
+print("\n")
+print("Giraffe\nAcademy")
+print("Giraffe \"Academy\"")
+
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[3])
+print(phrase.index("G"))
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
+print("\n")
+
+print(-7.097)
+print(3 * (3.5 + 5))
+print(10 / 3)
+print(10 % 3)
+
+my_num = -5
+print(my_num)
+print(str(my_num) + " my favorite number")
+print(abs(my_num))
+print(pow(3, 2))
+print(max(4, 6))
+print(min(4, 6))
+print(round(4.7))
+
+from math import *
+
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
